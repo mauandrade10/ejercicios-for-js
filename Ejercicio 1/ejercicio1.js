862e9f80da4ca5cc670a4b3c4246d05fba6673ca
Mauricio Andrade Prieto <mauricioandp@gmail.com>
2023-08-16 17:14:35 -0500

    This is teh first commit}

---
+++ Ejercicio 1/ejercicio1.js
@@ -0,0 +1,4 @@
+//1. Crea un programa que imprima en consola los números impares del 1 al 50.
+for(let i = 1; i<51; i++){
+    if(i%2 != 0) console.log(i);
+};
---
+++ Ejercicio 3/ejercicio3.js
@@ -0,0 +1,6 @@
+const array = [4,"dos",8,"tres",5,9,1,"cero"];
+
+for( const x of array){
+    if(typeof x == 'number') console.log(x)
+};
+
